4e40f310ede794e81c6ec47e2447ed8128171882
Igor Telles Barbosa <igor.telles@ibm.com>
2024-05-09 19:06:36 -0300

    compiling and quering fine

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
+import "reflect-metadata";
 import express from 'express';
 import { PrismaClient } from "@prisma/client"
+import { ApolloServer } from "apollo-server-express";
+import { resolvers } from "@generated/type-graphql";
+import { buildSchema } from 'type-graphql';
 
 const app = express();
 const prisma = new PrismaClient();
-const port = 3000;
 
 const main = async () => {
+    const port = process.env.PORT || 4000;
+
+    const schema = await buildSchema({
+        resolvers,
+        validate: false
+    })
+    const apollo = new ApolloServer({
+        schema,
+        context: ({ req }) => {
+            return { prisma, req }
+        }
+    });
+    await apollo.start();
+    apollo.applyMiddleware({ app: app as any });
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 }
+
 
 main()
     .then(async () => {
@@ -15,8 +37,4 @@ main()
     .catch(async (e) => {
         console.error(e);
         await prisma.$disconnect();
-    })
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    });
